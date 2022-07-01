1f630e4b55ba00f07645ae94471c5f7c39348aaa
yoesk8 <yoelgutierrez2021@yahoo.com>
2022-07-01 17:57:37 +0000

    Added tests and JS code for first 3 functions

---
+++ scripts/game.js
@@ -0,0 +1,21 @@
+let game = {
+    currentGame: [],
+    score: 0,
+    playerMoves: [],
+    choices: ["button1", "button2", "button3", "button4"]
+};
+
+function newGame() {
+
+    game.score = 0;
+    game.currentGame = [];
+    game.playerMoves = [];
+    showScore();
+    
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+ }
+
+module.exports = { game, newGame, showScore };
